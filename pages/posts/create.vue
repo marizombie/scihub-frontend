9989d1002f2f4f6bcc9dfbdcca1aa416d8ab77da
pasha <pashtet5246@gmail.com>
2023-12-11 22:00:48 +0100

    Make Ace create editor

--- pages/posts/create.vue
+++ pages/posts/create.vue
@@ -1,62 +1,271 @@
 <template>
-  <Editor api-key="dr6omthzv0unr7vp8ki2jf60qdrifdjn5wjjq3atlerdzczg" v-model="content" :init="{
-    plugins: 'print preview paste importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount imagetools textpattern noneditable help charmap quickbars emoticons',
-    menubar: 'file edit view insert format tools table help',
-    toolbar: 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample | ltr rtl',
-    toolbar_sticky: true,
-    autosave_ask_before_unload: true,
-    autosave_interval: '30s',
-    autosave_prefix: '{path}{query}-{id}-',
-    autosave_restore_when_empty: false,
-    autosave_retention: '2m',
-    image_advtab: true,
-    link_list: [
-      { title: 'My page 1', value: 'https://www.tiny.cloud' },
-      { title: 'My page 2', value: 'http://www.moxiecode.com' }
-    ],
-    image_list: [
-      { title: 'My page 1', value: 'https://www.tiny.cloud' },
-      { title: 'My page 2', value: 'http://www.moxiecode.com' }
-    ],
-    image_class_list: [
-      { title: 'None', value: '' },
-      { title: 'Some class', value: 'class-name' }
-    ],
-    importcss_append: true,
-    file_picker_callback: function (callback: Function, value: string, meta: any) {
-      /* Provide file and text for the link dialog */
-      if (meta.filetype === 'file') {
-        callback('https://www.google.com/logos/google.jpg', { text: 'My text' });
-      }
-
-      /* Provide image and alt text for the image dialog */
-      if (meta.filetype === 'image') {
-        callback('https://www.google.com/logos/google.jpg', { alt: 'My alt text' });
-      }
-
-      /* Provide alternative source and posted for the media dialog */
-      if (meta.filetype === 'media') {
-        callback('movie.mp4', { source2: 'alt.ogg', poster: 'https://www.google.com/logos/google.jpg' });
-      }
-    },
-    template_cdate_format: '[Date Created (CDATE): %m/%d/%Y : %H:%M:%S]',
-    template_mdate_format: '[Date Modified (MDATE): %m/%d/%Y : %H:%M:%S]',
-    height: 600,
-    image_caption: true,
-    quickbars_selection_toolbar: 'bold italic | quicklink h2 h3 blockquote quickimage quicktable',
-    noneditable_noneditable_class: 'mceNonEditable',
-    toolbar_mode: 'sliding',
-    contextmenu: 'link image imagetools table',
-    content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
-  }" />
-
-  {{ content }}
+  <div id="editorjs"></div>
 </template>
 
 <script setup lang="ts">
-import Editor from '@tinymce/tinymce-vue'
+import EditorJS from '@editorjs/editorjs';
+import Header from '@editorjs/header';
+import List from '@editorjs/list';
+import RawTool from '@editorjs/raw';
+import Quote from '@editorjs/quote';
+import ImageTool from '@editorjs/image';
+import Checklist from '@editorjs/checklist'
+import Embed from '@editorjs/embed';
+import LinkTool from '@editorjs/link';
+import Undo from 'editorjs-undo';
+import Warning from '@editorjs/warning';
+import Table from '@editorjs/table'
 
-let content = ref('');
+import AceCodeEditorJS, { AceCodeConfig } from "ace-code-editorjs";
+import ace from "ace-builds";
+import "ace-builds/esm-resolver";
+
+import modeHTMLWorker from "ace-builds/src-noconflict/worker-html?url";
+import modeJSWorker from "ace-builds/src-noconflict/worker-javascript?url";
+import modeCSSWorker from "ace-builds/src-noconflict/worker-css?url";
+import modePHPWorker from "ace-builds/src-noconflict/worker-php?url";
+import modeXMLWorker from "ace-builds/src-noconflict/worker-xml?url";
+import modeYamlWorker from "ace-builds/src-noconflict/worker-yaml?url";
+import modeLuaWorker from "ace-builds/src-noconflict/worker-lua?url";
+import modeJsonWorker from "ace-builds/src-noconflict/worker-json?url";
+
+ace.config.setModuleUrl("ace/mode/html_worker", modeHTMLWorker);
+ace.config.setModuleUrl("ace/mode/javascript_worker", modeJSWorker);
+ace.config.setModuleUrl("ace/mode/css_worker", modeCSSWorker);
+ace.config.setModuleUrl("ace/mode/php_worker", modePHPWorker);
+ace.config.setModuleUrl("ace/mode/xml_worker", modeXMLWorker);
+ace.config.setModuleUrl("ace/mode/yaml_worker", modeYamlWorker);
+ace.config.setModuleUrl("ace/mode/lua_worker", modeLuaWorker);
+ace.config.setModuleUrl("ace/mode/json_worker", modeJsonWorker);
+
+const aceConfig: AceCodeConfig = {
+  languages: {
+    plain: {
+      label: "Plain Text",
+      mode: "ace/mode/plain_text",
+    },
+    html: {
+      label: "HTML",
+      mode: "ace/mode/html",
+    },
+    javascript: {
+      label: "JavaScript",
+      mode: "ace/mode/javascript",
+    },
+    css: {
+      label: "CSS",
+      mode: "ace/mode/css",
+    },
+    php: {
+      label: "PHP",
+      mode: "ace/mode/php",
+    },
+    jsx: {
+      label: "JSX",
+      mode: "ace/mode/jsx",
+    },
+    tsx: {
+      label: "TSX",
+      mode: "ace/mode/tsx",
+    },
+    typescript: {
+      label: "TypeScript",
+      mode: "ace/mode/typescript",
+    },
+    sql: {
+      label: "SQL",
+      mode: "ace/mode/sql",
+    },
+    cpp: {
+      label: "C++",
+      mode: "ace/mode/c_cpp",
+    },
+    csharp: {
+      label: "C#",
+      mode: "ace/mode/csharp",
+    },
+    dart: {
+      label: "Dart",
+      mode: "ace/mode/dart",
+    },
+    django: {
+      label: "Django",
+      mode: "ace/mode/django",
+    },
+    dockerfile: {
+      label: "Docker",
+      mode: "ace/mode/dockerfile",
+    },
+    fsharp: {
+      label: "Docker",
+      mode: "ace/mode/fsharp",
+    },
+    golang: {
+      label: "Golang",
+      mode: "ace/mode/golang",
+    },
+    graphql: {
+      label: "GraphQL",
+      mode: "ace/mode/graphqlschema",
+    },
+    haskell: {
+      label: "Haskell",
+      mode: "ace/mode/haskell",
+    },
+    java: {
+      label: "Java",
+      mode: "ace/mode/java",
+    },
+    json: {
+      label: "Json",
+      mode: "ace/mode/json",
+    },
+    kotlin: {
+      label: "Kotlin",
+      mode: "ace/mode/kotlin",
+    },
+    less: {
+      label: "Less",
+      mode: "ace/mode/less",
+    },
+    lua: {
+      label: "PHP (Lua)",
+      mode: "ace/mode/lua",
+    },
+    makefile: {
+      label: "Makefile",
+      mode: "ace/mode/makefile",
+    },
+    mysql: {
+      label: "MySQL",
+      mode: "ace/mode/mysql",
+    },
+    nginx: {
+      label: "NginX",
+      mode: "ace/mode/nginx",
+    },
+    objectivec: {
+      label: "Objective C",
+      mode: "ace/mode/objectivec",
+    },
+    pascal: {
+      label: "Pascal",
+      mode: "ace/mode/pascal",
+    },
+    perl: {
+      label: "Perl",
+      mode: "ace/mode/perl",
+    },
+    pgsql: {
+      label: "Postgres",
+      mode: "ace/mode/pgsql",
+    },
+    laravel: {
+      label: "PHP (Laravel)",
+      mode: "ace/mode/php_laravel_blade",
+    },
+    prisma: {
+      label: "Prisma",
+      mode: "ace/mode/prisma",
+    },
+    python: {
+      label: "Python",
+      mode: "ace/mode/python",
+    },
+    r: {
+      label: "R",
+      mode: "ace/mode/r",
+    },
+    ruby: {
+      label: "Ruby",
+      mode: "ace/mode/ruby",
+    },
+    rust: {
+      label: "Rust",
+      mode: "ace/mode/rust",
+    },
+    sass: {
+      label: "Sass",
+      mode: "ace/mode/sass",
+    },
+    scss: {
+      label: "Scss",
+      mode: "ace/mode/scss",
+    },
+    swift: {
+      label: "Swift",
+      mode: "ace/mode/swift",
+    },
+    terraform: {
+      label: "Terraform",
+      mode: "ace/mode/terraform",
+    },
+    xml: {
+      label: "XML",
+      mode: "ace/mode/xml",
+    },
+    yaml: {
+      label: "YAML",
+      mode: "ace/mode/yaml",
+    },
+  },
+  options: {
+    fontSize: 16,
+    minLines: 4,
+    theme: "ace/theme/monokai",
+    enableAutoIndent: true,
+    useSoftTabs: true,
+    customScrollbar: true
+  },
+};
+
+const editor = new EditorJS({
+
+  holder: 'editorjs',
+  tools: {
+    header: Header,
+    list: List,
+    raw: RawTool,
+    quote: Quote,
+    image: {
+      class: ImageTool,
+      config: {
+        endpoints: {
+          byFile: 'http://localhost:8008/uploadFile', // Your backend file uploader endpoint
+          byUrl: 'http://localhost:8008/fetchUrl', // Your endpoint that provides uploading by Url
+        }
+      }
+    },
+    checklist: {
+      class: Checklist,
+      inlineToolbar: true,
+    },
+    embed: Embed,
+    linkTool: {
+      class: LinkTool,
+      config: {
+        endpoint: 'http://localhost:8008/fetchUrl', // Your backend endpoint for url data fetching,
+      }
+    },
+    warning: Warning,
+    table: Table,
+    code: {
+      class: AceCodeEditorJS,
+      config: aceConfig,
+    },
+  },
+  onReady: () => {
+    new Undo({ editor });
+  },
+})
+
+
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+#editorjs {
+  width: 100%;
+  max-width: 800px;
+  box-shadow: 0px 2px 1px -1px var(--v-shadow-key-umbra-opacity, rgba(0, 0, 0, 0.2)), 0px 1px 1px 0px var(--v-shadow-key-penumbra-opacity, rgba(0, 0, 0, 0.14)), 0px 1px 3px 0px var(--v-shadow-key-penumbra-opacity, rgba(0, 0, 0, 0.12));
+}
+</style>
