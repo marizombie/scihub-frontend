84c1f5ebdab0f860f928617b66524469c7986a37
pasha <pashtet5246@gmail.com>
2024-05-22 17:54:05 +0200

    CNS-259 Make ace code style to z-index: 0

--- pages/posts/create.vue
+++ pages/posts/create.vue
@@ -648,6 +648,10 @@ function redirectToDrafts() {
     0px 2px 1px -1px var(--v-shadow-key-umbra-opacity, rgba(0, 0, 0, 0.2)),
     0px 1px 1px 0px var(--v-shadow-key-penumbra-opacity, rgba(0, 0, 0, 0.14)),
     0px 1px 3px 0px var(--v-shadow-key-penumbra-opacity, rgba(0, 0, 0, 0.12));
+
+  :deep(.ace_gutter) {
+    z-index: 0;
+  }
 }
 
 .container-create {
